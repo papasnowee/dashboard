7dbd04755ff6fcf25ac1080dca6ff911473ceca9
jilkinalexandr <jilkinalexandr@gmail.com>
2021-04-03 12:00:20 +0300

    fixed bug related app-crushing. The default value of currencies '1' --> 'USD'.

--- src/components/userSettings/currency/Currency.jsx
+++ src/components/userSettings/currency/Currency.jsx
@@ -1,40 +1,38 @@
-import React,{useContext} from 'react';
+import React, { useContext, useMemo } from 'react';
 import HarvestContext from '../../../Context/HarvestContext';
-import {CurrencyContainer} from './CurrencyStyles';
-
+import { CurrencyContainer } from './CurrencyStyles';
 
 const Currency = () => {
-    const {
-        exchangeRates,
-        baseCurrency,
-        setBaseCurrency,
-        setCurrentExchangeRate,
-      } = useContext(HarvestContext);
+  const { exchangeRates, baseCurrency, setBaseCurrency, setCurrentExchangeRate } = useContext(
+    HarvestContext,
+  );
 
-    const changeHandler = (e) => {
-        setBaseCurrency(e.target.value);
-        setCurrentExchangeRate(exchangeRates[e.target.value]);
-        window.localStorage.setItem("HarvestFinance:currency", e.target.value)
-      };
-    return (
-        <CurrencyContainer>
-        <h3>Display currency in:</h3>
-        <select
-          onChange={changeHandler}
-          value={baseCurrency}
-          name="currency"
-          id="currencies"
-        >
-          <option value="1">{baseCurrency}</option>
+  const changeHandler = e => {
+    setBaseCurrency(e.target.value);
+    setCurrentExchangeRate(exchangeRates[e.target.value]);
+    window.localStorage.setItem('HarvestFinance:currency', e.target.value);
+  };
 
-          {exchangeRates
-            ? Object.entries(exchangeRates).map(([key, value]) => {
-                return <option key={key} value={key}>{key}</option>;
-              })
-            : ""}
-        </select>
-      </CurrencyContainer>
-    );
-}
+  const options = useMemo(() => {
+    return Object.entries(exchangeRates).map(([key]) => {
+      return (
+        <option key={key} value={key}>
+          {key}
+        </option>
+      );
+    });
+  }, [exchangeRates]);
+
+  return (
+    <CurrencyContainer>
+      <h3>Display currency in:</h3>
+      <select onChange={changeHandler} value={baseCurrency} name="currency" id="currencies">
+        <option value="USD">{baseCurrency}</option>
+
+        {exchangeRates && options}
+      </select>
+    </CurrencyContainer>
+  );
+};
 
 export default Currency;
